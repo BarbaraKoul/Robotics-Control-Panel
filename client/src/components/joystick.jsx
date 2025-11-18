a6880f53f741af1054a5ed1258a25c8c1598c52a
Barbara <barbarakoulour@gmail.com>
2025-11-18 23:25:09 +0200

    joystick design updated

--- client/src/components/joystick.jsx
+++ client/src/components/joystick.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Joystick } from 'react-joystick-component'
+import baseImage from '../images/joystick.png'
+import dotImage from '../images/glowyDot.png'
 
 function JoystickControl() {
   const handleMove = (event) => {
@@ -21,17 +23,24 @@ function JoystickControl() {
     })
   }
 
-  return (
-    <div className='fixed right-2.5 bottom-10'>
-      <h2 className='text-center text-xl font-[Roboto]'>Joystick</h2>
+  return (<>
+  <div className='absolute bottom-50 right-52'>
+    <div className='flex items-center space-x-2 bottom-50 right-60'>
+      <img src={dotImage} className='w-4 h-4]'></img>
+      <h2 className='text-center text-xl font-[Roboto] glowBlue'>Control Panel</h2>
+    </div>
+  </div>
+  <div className='fixed right-10 bottom-10 bg-panel w-80 h-40 pt-4 pl-2 rounded-2xl'>
       <Joystick
-        size={150}
-        baseColor='rgba(210, 206, 206, 1)'
-        stickColor='rgb(175,228,227)'
+        size={130}
+        stickSize={50}
+        baseImage={baseImage}
+        stickImage={baseImage}
         move={handleMove}
         stop={handleStop}
       />
     </div>
+  </>
   )
 }
 
